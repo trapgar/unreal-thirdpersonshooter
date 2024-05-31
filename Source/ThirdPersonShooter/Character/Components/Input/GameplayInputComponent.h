19a19bcce014d3946101406cb10f523654948147
Tyler <trapgar@gmail.com>
2024-05-31 00:24:36 -0500

    fixed bug with GameplayInputComponent not exporting

--- Source/ThirdPersonShooter/Character/Components/Input/GameplayInputComponent.h
+++ Source/ThirdPersonShooter/Character/Components/Input/GameplayInputComponent.h
@@ -18,7 +18,7 @@ class UObject;
  *	Component used to manage input mappings and bindings using an input config data asset.
  */
 UCLASS(Config = Input)
-class UGameplayInputComponent : public UEnhancedInputComponent
+class THIRDPERSONSHOOTER_API UGameplayInputComponent : public UEnhancedInputComponent
 {
 	GENERATED_BODY()
 
